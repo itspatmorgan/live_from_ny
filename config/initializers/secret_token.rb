25f29954c81b552a0c3892d2abbc69de1065faaa
Patrick Morgan <pamorgan@Macintosh-4.local>
2014-01-13 14:05:45 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveFromNy::Application.config.secret_token = 'c91cd85231889bd0462cec9c8201498e8cb5cd93994e0e25a11dd82df4c527ab570750c3e329abe79ec7933a80dc9a5c3a63807a0f9f90945ae73e8360274507'
