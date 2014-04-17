d166a0a24228fff5a5100306d1b4f4629f1fa2c7
Patrick Morgan <pamorgan12@gmail.com>
2014-04-17 09:41:46 -0400

    updating the specs

--- spec/features/articles_features_spec.rb
+++ spec/features/articles_features_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe ArticlesController do
-  describe "Retrieve articles from the API" do
-    describe "Starting on the home page and given a date" do
+  describe "retrieve articles from the API" do
+    context "starting on the home page with a given a date" do
       before do
         @date = 19900212
         visit root_path
       end
-      it "can return articles from NYTIMES API based on a given date" do
+      it "returns articles from NYTIMES API based on that date" do
         fill_in :date, with: @date
         click_button "submit"
         
@@ -22,7 +22,7 @@ describe ArticlesController do
     end
   end
   describe "logged-in user can save an article" do
-    describe "Given a logged-in user and an article" do
+    context "given a logged-in user and an article on the articles index path" do
       before do
         @user = User.create({
             email: 'pat@example.com', 
@@ -35,22 +35,19 @@ describe ArticlesController do
           lead_paragraph: 'I am a lead paragraph',
           url: 'example.com',
         })
+        @date = 19900212
+        @index = 0
+        visit "/articles?date=#{@date}"
+        click_button "like_#{@index}"
       end
-      describe "Starting with a user on the articles index path" do
-        before do
-          @date = 19900212
-          @index = 0
-          visit "/articles?date=#{@date}"
-          click_button "like_#{@index}"
-        end
-        it "should create a like when button is clicked" do
-          @user.articles << @article
-          @user.articles.should include Article.find(@article.id)
-        end
-        it "should show headline on user show page" do
-          visit user_path(@user)
-          page.should have_content(@article.headline)
-        end
+      it "creates a like when button is clicked" do
+        @user.articles << @article
+        @user.articles.should include Article.find(@article.id)
+      end
+      it "shows headline on user show page" do
+        visit user_path(@user)
+        page.should have_content(@article.headline)
+      end
       end
     end
   end
